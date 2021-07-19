a7def2c319da8a3cf9df7c9ca4f477e2eb9657fa
Fran Canete <francanetedev@gmail.com>
2021-07-19 21:44:07 +0100

    Added started files

---
+++ js/script.js
@@ -0,0 +1,33 @@
+/************************************************
+Treehouse Techdegree:
+Project 4 - Random Quote Generator in JavaScript
+*************************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://uboraacademy.slack.com
+
+/*** 
+ * `quotes` array 
+***/
+
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
