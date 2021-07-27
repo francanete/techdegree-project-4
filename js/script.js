43ddb6c6246bddfb1030dfe0ae559a6d309dfde9
Fran Canete <francanetedev@gmail.com>
2021-07-27 11:47:02 +0100

    Added array of objects to store quotes

--- js/script.js
+++ js/script.js
@@ -12,6 +12,41 @@ Project 4 - Random Quote Generator in JavaScript
 ***/
 
 
+let quotes = [
+  {
+    "quote": "The season of failure is the best time for sowing the seeds of success.",
+    "source": "Yogananda",
+    "citation": "Autobiography of a yogi",
+    "year": 1946
+  },
+  {
+    "quote": "Sometimes life hits you in the head with a brick. Don't lose faith.",
+    "source": "Steve Jobs",
+    "citation": "Stanford Commencement Address",
+    "year": 2005
+  },
+  {
+    "quote": "Fear is the path to the dark side. Fear leads to anger. Anger leads to hate. Hate leads to suffering.",
+    "source": "Yoda",
+    "citation": "Star Wars: The Phantom Menace",
+    "year": 1999
+  },
+  {
+    "quote": "All the world is made of faith, and trust, and pixie dust.",
+    "source": "J.M. Barrie",
+    "citation": "Peter Pan",
+    "year": 1911
+  },
+  {
+    "quote": "Enthusiasm makes up for a host of deficiencies.",
+    "source": "Barack Obama",
+    "citation": "A Promised Land",
+    "year": 2020
+  }
+];
+
+console.log(quotes)
+
 
 /***
  * `getRandomQuote` function
