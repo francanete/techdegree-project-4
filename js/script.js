d5c1b1f2032178d96519ef79c3d673cf73b51ded
Fran Canete <francanetedev@gmail.com>
2021-07-28 08:52:06 +0100

    Added tag property to the quotes object

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ Project 4 - Random Quote Generator in JavaScript
   // Reach out in your Slack community - https://uboraacademy.slack.com
 
 /*** 
- * `quotes` array 
+ * `quotes` array: stores a series of quotes that will be use to display randomely
 ***/
 
 
@@ -17,38 +17,42 @@ let quotes = [
     "quote": "The season of failure is the best time for sowing the seeds of success.",
     "source": "Yogananda",
     "citation": "Autobiography of a yogi",
-    "year": 1946
+    "year": 1946,
+    "tag": "Mindset"
   },
   {
     "quote": "Sometimes life hits you in the head with a brick. Don't lose faith.",
     "source": "Steve Jobs",
     "citation": "Stanford Commencement Address",
-    "year": 2005
+    "year": 2005,
+    "tag": "Mindset"
   },
   {
     "quote": "Fear is the path to the dark side. Fear leads to anger. Anger leads to hate. Hate leads to suffering.",
     "source": "Yoda",
     "citation": "Star Wars: The Phantom Menace",
-    "year": 1999
+    "year": 1999,
+    "tag": "Movies"
   },
   {
     "quote": "All the world is made of faith, and trust, and pixie dust.",
     "source": "J.M. Barrie",
     "citation": "Peter Pan",
-    "year": 1911
+    "year": 1911,
+    "tag": "Books"
   },
   {
     "quote": "Enthusiasm makes up for a host of deficiencies.",
     "source": "Barack Obama",
     "citation": "A Promised Land",
-    "year": 2020
+    "year": 2020,
+    "tag": "Life"
   }
 ];
 
 
-
 /***
- * `getRandomQuote` function
+ * `getRandomQuote` function: generates a random number based on the index array and create an object with the quote
 ***/
 
 function getRandomQuote() {
@@ -56,13 +60,22 @@ function getRandomQuote() {
   return quotes[randomQuoteNumber];
 }
 
+/***
+ *  Generates a random color to be inserted into the printQuote() fuction
+ */
+
+function newBackground() {
+  let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+  return document.body.style.background = randomColor;
+}
 
 /***
- * `printQuote` function
+ * `printQuote` function: get the object property and value and create a block of HTML. Aditionally, creates a random color any time quote is printed.
 ***/
 
 
-function printQuote () {
+
+function printQuote() {
   let randomQuote = getRandomQuote();
   let randomQuoteHTML = `<p class="quote"> ${randomQuote.quote} </p><p class="source"> ${randomQuote.source}`;
     if ( randomQuote.citation ) {
@@ -71,19 +84,22 @@ function printQuote () {
     if ( randomQuote.year ) {
       randomQuoteHTML += `<span class="year">${randomQuote.year}</span>`;
     }
+    if ( randomQuote.tag ) {
+      randomQuoteHTML += `<br><span class="tag">${randomQuote.tag}</span>`;
+    }
     randomQuoteHTML += "</p>";
-  return document.getElementById('quote-box').innerHTML = randomQuoteHTML;
-
-
-  // console.log(randomQuote);
-  // console.log(randomQuoteHTML);
+  return document.getElementById('quote-box').innerHTML = randomQuoteHTML, newBackground();
 }
 
 
+  /***
+   * Displays a random quote every 10 seconds
+   ***/
 
 
-
-printQuote();
+ setInterval(function(){
+   printQuote();
+ }, 10000);
 
 
 
